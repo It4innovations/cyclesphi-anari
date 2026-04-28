89c441034598b3c1b1b1085d744432bde1b185fe
Milan Jaros <jar091@vsb.cz>
2026-04-28 10:57:59 +0200

    update cyclesphi; fix volumeloader

--- device/VolumeImageLoader.cpp
+++ device/VolumeImageLoader.cpp
@@ -65,33 +65,25 @@ void VolumeImageLoader::get_transform(Transform &world_to_index) {
 
 bool VolumeImageLoader::load_metadata(ImageMetaData &metadata)
 {  
-
-
-  // TODO: MJ
-  //metadata.transform_3d = //transform_identity();
-  //   ccl::transform_scale(ccl::make_float3(1.f / p_field->m_dims[0],
-  //       1.f / p_field->m_dims[1],
-  //       1.f / p_field->m_dims[2]));
-
-  // TODO: MJ
   Transform index_to_object;
-  get_transform(index_to_object);
+  //get_transform(index_to_object);
   //index_to_object = transform_inverse(index_to_object);
-  metadata.transform_3d = index_to_object;
+  //metadata.transform_3d = index_to_object;
+  //metadata.use_transform_3d = false;
+
+  metadata.transform_3d = ccl::transform_scale(ccl::make_float3(
+      (float)p_field->m_dims[0],
+      (float)p_field->m_dims[1],
+      (float)p_field->m_dims[2]));
 
   metadata.use_transform_3d = false;
 
   auto byte_size = p_field->m_data->totalSize()
       * anari::sizeOf(p_field->m_data->elementType());
-  metadata.width = byte_size;
-  //p_field->m_dims[0];
-  metadata.height = 1;
-  //p_field->m_dims[1];
-  //metadata.depth = p_field->m_dims[2];
-#if 0
+  metadata.nanovdb_byte_size = byte_size;
+
   metadata.width = p_field->m_dims[0];
   metadata.height = p_field->m_dims[1];
-#endif
 
   switch (p_field->m_data->elementType()) {
   case (ANARI_FLOAT32):
@@ -159,11 +151,11 @@ float3 VolumeImageLoader::index_to_world(float3 index)
 {
     return make_float3((float)index[0], (float)index[1], (float)index[2]);
 
-    Transform index_to_object;
-    get_transform(index_to_object);
-    index_to_object = transform_inverse(index_to_object);
+    //Transform index_to_object;
+    //get_transform(index_to_object);
+    //index_to_object = transform_inverse(index_to_object);
 
-    return ccl::transform_point(&index_to_object, index);
+    //return ccl::transform_point(&index_to_object, index);
 
     // Transform from voxel index to world coordinates
     // world_pos = origin + index * spacing
